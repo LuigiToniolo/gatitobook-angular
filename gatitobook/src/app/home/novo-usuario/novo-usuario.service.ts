da7d5c8cbbf98fc8b615b502f340c51321bb7576
LuigiToniolo <luigi.toniolo@domvsit.com.br>
2022-08-05 18:07:24 -0300

    cadastrando usuário e fazendo login com novo cadastro

--- gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -7,11 +7,16 @@ import { NovoUsuario } from './novo-usuario';
 })
 export class NovoUsuarioService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   //Recebe um novo usuário e retorna o observable com sucesso ou falha em relação ao post:
 
   cadastraNovoUsuario(novoUsuario: NovoUsuario) {
     return this.http.post('http://localhost:3000/user/signup', novoUsuario);
   }
+
+  verificaUsuarioExistente(nomeUsuario: string) {
+    return this.http.get(`http://localhost:3000/user/exists/${nomeUsuario}`);
+  }
+
 }
